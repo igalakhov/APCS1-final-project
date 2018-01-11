5f0403f9f1478a4d9227bcad8b1278bc5f217221
Ivan Galakhov <igalakhov00@stuy.edu>
2018-01-11 12:14:27 -0500

    Modified start code

--- src/TwitterShell.java
+++ src/TwitterShell.java
@@ -7,16 +7,11 @@ public class TwitterShell {
 	    System.out.println("Welcome to the twitter shell!");
 	    System.out.println("Created By Ivan Galakhov, Jan. 2018");
 
-	    //test with user input
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            System.out.print("> ");
-            String in = scanner.nextLine();
-            if(in.equals("exit")){
-                break;
-            }
-        }
+	    //run the shell
+        CommandHandler mainHandler = new CommandHandler(new Scanner(System.in));
+        mainHandler.run();
 
-
+        //we should never get here because mainHandler.run() exits the program
+        System.out.println("If you see this, something went terribly wrong");
     }
 }
