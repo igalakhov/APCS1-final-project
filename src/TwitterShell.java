ba025c848300be92dc8f9670f3329ebfa3c24525
Ivan Galakhov <igalakhov00@stuy.edu>
2018-01-12 16:38:37 -0500

    Libraries Work now

--- src/TwitterShell.java
+++ src/TwitterShell.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classification;
+import twitter4j.*;
+
 public class TwitterShell {
     public static void main(String[] args) {
 
@@ -7,10 +10,11 @@ public class TwitterShell {
 	    System.out.println("Welcome to the twitter shell!");
 	    System.out.println("Created By Ivan Galakhov, Jan. 2018");
 
-
 	    //run the shell
         CommandHandler mainHandler = new CommandHandler(new Scanner(System.in));
         mainHandler.run();
+
+
 
         //we should never get here because mainHandler.run() exits the program
         System.out.println("If you see this, something went terribly wrong");
